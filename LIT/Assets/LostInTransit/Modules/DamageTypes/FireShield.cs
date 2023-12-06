b702444ff2d53d76f037388ce1e361bfc6cf21dc
swuff-star <63352264+swuff-star@users.noreply.github.com>
2023-12-06 00:57:42 -0500

    fire shield finish + vfx
    ^ idk what else to say. could maybe use more tweaking but fully functional as is

---
+++ LIT/Assets/LostInTransit/Modules/DamageTypes/FireShield.cs
@@ -0,0 +1,46 @@
+﻿using Moonstorm;
+using R2API;
+using RoR2;
+using UnityEngine;
+using UnityEngine.Networking;
+using static R2API.DamageAPI;
+
+namespace LostInTransit.DamageTypes
+{
+    public sealed class FireShield : DamageTypeBase
+    {
+        public override ModdedDamageType ModdedDamageType { get; protected set; }
+
+        public static ModdedDamageType fireShield;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            fireShield = ModdedDamageType;
+        }
+
+        public override void Delegates()
+        {
+            GlobalEventManager.onServerDamageDealt += Ignite;
+        }
+
+        private void Ignite(DamageReport report)
+        {
+            CharacterBody attackerBody = report.attackerBody;
+            DamageInfo damageInfo = report.damageInfo;
+            if (DamageAPI.HasModdedDamageType(damageInfo, ModdedDamageType))
+            {
+                var dotInfo = new InflictDotInfo()
+                {
+                    attackerObject = attackerBody.gameObject,
+                    victimObject = report.victim.gameObject,
+                    dotIndex = DotController.DotIndex.Burn,
+                    duration = 2f,
+                    damageMultiplier = attackerBody.GetItemCount(LITContent.Items.FireShield) * Items.FireShield.burnCoef
+                };
+                StrengthenBurnUtils.CheckDotForUpgrade(report.attackerBody.inventory, ref dotInfo);
+                DotController.InflictDot(ref dotInfo);
+            }
+        }
+    }
+}
--- LIT/Assets/LostInTransit/Modules/Pickups/Items/FireShield.cs
+++ LIT/Assets/LostInTransit/Modules/Pickups/Items/FireShield.cs
@@ -4,14 +4,23 @@ using RoR2.Items;
 using UnityEngine;
 using R2API;
 using System;
+using UnityEngine.AddressableAssets;
 
 namespace LostInTransit.Items
 {
-    [DisabledContent]
+    //[DisabledContent]
     public class FireShield : ItemBase
     {
         private const string token = "LIT_ITEM_FIRESHIELD_DESC";
         public override ItemDef ItemDef { get; } = LITAssets.LoadAsset<ItemDef>("FireShield", LITBundle.Items);
+
+        [ConfigurableField(ConfigName = "Base Damage Coefficient", ConfigDesc = "Base damage dealt by Fire Shield.")]
+        [TokenModifier(token, StatTypes.Default, 1)]
+        public static float dmgCoef = 3f;
+
+        [ConfigurableField(ConfigName = "Burn Damage Coefficient", ConfigDesc = "Added burn damage per stack.")]
+        [TokenModifier(token, StatTypes.Default, 1)]
+        public static float burnCoef = 1f;
 
         //to-do: stacking, config
 
@@ -19,18 +28,19 @@ namespace LostInTransit.Items
         {
             [ItemDefAssociation(useOnClient = true, useOnServer = true, behaviorTypeOverride = typeof(FireShieldBehavior))]
             public static ItemDef GetItemDef() => LITContent.Items.FireShield;
+            private BlastAttack blastAttack;
 
             public void OnIncomingDamageServer(DamageInfo damageInfo)
             {
                 if (damageInfo.damage >= (body.maxHealth / 10))
                 {
-                    new BlastAttack
+                    blastAttack = new BlastAttack()
                     {
                         position = body.corePosition,
-                        baseDamage = body.damage * 3f,
-                        baseForce = 3000f,
-                        bonusForce = Vector3.up * 1500f,
-                        radius = 8f,
+                        baseDamage = body.damage * dmgCoef,
+                        baseForce = 2000f,
+                        bonusForce = Vector3.up * 750f,
+                        radius = 2f,
                         attacker = body.gameObject,
                         inflictor = body.gameObject,
                         crit = Util.CheckRoll(body.crit, body.master),
@@ -38,9 +48,20 @@ namespace LostInTransit.Items
                         falloffModel = BlastAttack.FalloffModel.Linear,
                         attackerFiltering = AttackerFiltering.NeverHitSelf,
                         teamIndex = body.teamComponent.teamIndex,
-                        damageType = DamageType.IgniteOnHit,
-                        procCoefficient = 1.0f,
-                    }.Fire();
+                        procCoefficient = 1.0f
+                    };
+                    DamageAPI.AddModdedDamageType(blastAttack, DamageTypes.FireShield.fireShield);
+                    blastAttack.Fire();
+
+                    //EffectManager.SimpleEffect(Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Common/VFX/ExplosionVFX.prefab").WaitForCompletion(), body.transform.position, Quaternion.identity.normalized, true);
+
+                    EffectData effectData = new EffectData
+                    {
+                        origin = body.corePosition,
+                        scale = 4.5f,
+                        rotation = new Quaternion(90, 0, 0, 0)
+                    };
+                    EffectManager.SpawnEffect(Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Common/VFX/ExplosionVFX.prefab").WaitForCompletion(), effectData, true);
                 }
             }
         }
