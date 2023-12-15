dc1e72f6371e68f3020872fe4e451be4ca65a035
Nebby <nebby131999@gmail.com>
2023-12-14 22:59:14 -0300

    Implementedd blighted elite blacklist

--- LIT/Assets/LostInTransit/Modules/Elites/BlightedElites.cs
+++ LIT/Assets/LostInTransit/Modules/Elites/BlightedElites.cs
@@ -2,6 +2,7 @@
 using Moonstorm;
 using R2API;
 using RoR2;
+using RoR2.ExpansionManagement;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -20,10 +21,19 @@ namespace LostInTransit.Elites
         public static ReadOnlyCollection<EliteDef> ElitesHonorEnabled { get; private set; }
         private static List<EliteDef> _elitesHonorEnabled = new List<EliteDef>();
 
+        public static ReadOnlyCollection<BodyIndex> PrestigeBodyBlacklist { get; private set; }
+        private static List<CharacterBody> _prestigeBodyBlacklist = new List<CharacterBody>();
+
+        public static ReadOnlyCollection<BodyIndex> RegularBodyBlacklist { get; private set; }
+        private static List<CharacterBody> _regularBodyBlacklist = new List<CharacterBody>();
+
+        public static void AddBodyToRegularBlacklist(CharacterBody body) => _regularBodyBlacklist.Add(body);
+        public static void AddBodyToPrestigeBlacklist(CharacterBody body) => _prestigeBodyBlacklist.Add(body);
+
         public static void AddEliteToHonorEnabledList(EliteDef eliteDef) => _elitesHonorEnabled.Add(eliteDef);
         public static void AddEliteToHonorDisabledList(EliteDef eliteDef) => _elitesHonorDisabled.Add(eliteDef);
 
-        [SystemInitializer(typeof(EliteCatalog))]
+        [SystemInitializer(typeof(EliteCatalog), typeof(BodyCatalog))]
         private static void Initialize()
         {
             if (!EliteCatalog.eliteDefs.Contains(LITAssets.LoadAsset<MSEliteDef>("Blighted", LITBundle.Equips)))
@@ -33,7 +43,7 @@ namespace LostInTransit.Elites
             LITLog.Info("Blighted elites enabled, setting up underlying systems...");
             Initialized = true;
 
-            RoR2Application.onLoad += AddDefaultElites;
+            RoR2Application.onLoad += AddDefaultCollectionEntries;
             Run.onRunStartGlobal += CreateFinalizedCollections;
             On.RoR2.Util.GetBestBodyName += MakeBlightedName;
         }
@@ -65,7 +75,13 @@ namespace LostInTransit.Elites
             return text;
         }
 
-        private static void AddDefaultElites()
+        private static void AddDefaultCollectionEntries()
+        {
+            AddElitesToLists();
+            AddBodiesToLists();
+        }
+
+        private static void AddElitesToLists()
         {
             AddEliteToHonorDisabledList(RoR2Content.Elites.Fire);
             AddEliteToHonorDisabledList(RoR2Content.Elites.Lightning);
@@ -82,36 +98,204 @@ namespace LostInTransit.Elites
             AddEliteToHonorEnabledList(LITContent.Elites.VolatileHonor);
         }
 
+        private static void AddBodiesToLists()
+        {
+            //Altar Skeleton
+            AddToBothBlacklists("AltarSkeleton");
+
+            //Reliquary
+            AddToBothBlacklists("ArtifactShell");
+
+            //Birdshark
+            AddToBothBlacklists("Birdshark");
+
+            //N- Id like to have mmtirhix have a chance to be blighted with prestige for the funny, but i cant be bothered atm to make the hurt and haunt body blighted only if the original body was.
+            AddToBothBlacklists("Brother");
+            AddToBothBlacklists("BrotherHurt");
+            AddToBothBlacklists("BrotherHaunt");
+
+            //Drones and turret
+            AddToBothBlacklists("BackupDrone");
+            AddToBothBlacklists("Drone1");
+            AddToBothBlacklists("Drone2");
+            AddToBothBlacklists("EmergencyDrone");
+            AddToBothBlacklists("EquipmentDrone");
+            AddToBothBlacklists("FlameDrone");
+            AddToBothBlacklists("MegaDrone");
+            AddToBothBlacklists("MissileDrone");
+            AddToBothBlacklists("Turret1");
+            AddToBothBlacklists("DroneCommander");
+
+            //Worms
+            AddToBothBlacklists("ElectricWorm");
+            AddToBothBlacklists("MagmaWorm");
+
+
+            //Engi's Turrets, They inherit the equipment anyways so if the og engi is blighted then the others will
+            AddToBothBlacklists("EngiTurret");
+            AddToBothBlacklists("EngiWalkerTurret");
+
+            //Explosive barrels
+            AddToBothBlacklists("ExplosivePotDestructible");
+            AddToBothBlacklists("FusionCellDestructible");
+            AddToBothBlacklists("SulfurPod");
+
+            //Chimera
+            AddToBothBlacklists("LunarExploder");
+            AddToBothBlacklists("LunarGolem");
+            AddToBothBlacklists("LunarWisp");
+
+            //Voids...? i'll leave them commented for now
+            /*AddToBothBlacklists("Nullifier");
+            AddToBothBlacklists("VoidBarnacle");
+            AddToBothBlacklists("VoidJailer");
+            AddToBothBlacklists("VoidMegaCrab");*/
+
+            //Void Allies...? i'll leave them commented for now
+            /*AddToBothBlacklists("NullifierAlly");
+            AddToBothBlacklists("VoidJailerAlly");
+            AddToBothBlacklists("VoidMmegaCrabAlly");*/
+
+            //Voidling and its morbillion phases
+            AddToBothBlacklists("MiniVoidRaidCrabBodyBase");
+            AddToBothBlacklists("MiniVoidRaidCrabBodyPhase1");
+            AddToBothBlacklists("MiniVoidRaidCrabBodyPhase2");
+            AddToBothBlacklists("MiniVoidRaidCrabBodyPhase3");
+            AddToBothBlacklists("VoidRaidCrab");
+
+            //Void Infestor
+            AddToBothBlacklists("VoidInfestor");
+
+            //Newt
+            AddToBothBlacklists("Shopkeeper");
+
+            //Spectators
+            AddToBothBlacklists("Spectator");
+            AddToBothBlacklists("SpectatorSlow");
+
+            //Malachite Urchin
+            AddToBothBlacklists("UrchinTurret");
+
+            //Mending Heal Bomb
+            AddToBothBlacklists("AffixEarthHealer");
+
+            //Time Crystals
+            AddToBothBlacklists("TimeCrystal");
+
+            //Roboball buddies
+            AddToBothBlacklists("RoboBallRedBuddy");
+            AddToBothBlacklists("RoboBallGreenBuddy");
+
+            //Gland Beetle Guard
+            AddToBothBlacklists("BeetleGuardAlly");
+
+            //Alpha Construct ally
+            AddToBothBlacklists("MinorConstructOnKill");
+
+            //Squid Turrets
+            AddToBothBlacklists("SquidTurret");
+
+            //Geep
+            AddToBothBlacklists("Geep");
+            //Gip
+            AddToBothBlacklists("Gip");
+
+            void AddToBothBlacklists(string name)
+            {
+                AddBodyToPrestigeBlacklist(GetBody(name));
+                AddBodyToRegularBlacklist(GetBody(name));
+            }
+            CharacterBody GetBody(string name)
+            {
+                return BodyCatalog.GetBodyPrefabBodyComponent(BodyCatalog.FindBodyIndexCaseInsensitive(name.Contains("Body") ? name : name + "Body"));
+            }
+        }
+
         private static void CreateFinalizedCollections(Run obj)
+        {
+            CreateEliteCollections(obj);
+            CreateBlacklistCollections(obj);
+        }
+
+        private static void CreateEliteCollections(Run run)
         {
             var honorDisabledCollection = new List<EliteDef>();
             var honorEnabledCollection = new List<EliteDef>();
 
-            foreach(var nonHonorElite in _elitesHonorDisabled)
+            foreach (var nonHonorElite in _elitesHonorDisabled)
             {
-                if(!nonHonorElite.eliteEquipmentDef.requiredExpansion)
+                if (nonHonorElite.eliteIndex == EliteIndex.None)
+                    continue;
+
+                if (!nonHonorElite.eliteEquipmentDef.requiredExpansion)
                 {
                     honorDisabledCollection.Add(nonHonorElite);
                     continue;
                 }
 
-                if (obj.IsExpansionEnabled(nonHonorElite.eliteEquipmentDef.requiredExpansion))
+                if (run.IsExpansionEnabled(nonHonorElite.eliteEquipmentDef.requiredExpansion))
                     honorDisabledCollection.Add(nonHonorElite);
             }
-            foreach(var honorElite in _elitesHonorEnabled)
+            foreach (var honorElite in _elitesHonorEnabled)
             {
-                if(!honorElite.eliteEquipmentDef.requiredExpansion)
+                if (honorElite.eliteIndex == EliteIndex.None)
+                    continue;
+
+                if (!honorElite.eliteEquipmentDef.requiredExpansion)
                 {
                     honorEnabledCollection.Add(honorElite);
                     continue;
                 }
 
-                if (obj.IsExpansionEnabled(honorElite.eliteEquipmentDef.requiredExpansion))
+                if (run.IsExpansionEnabled(honorElite.eliteEquipmentDef.requiredExpansion))
                     honorEnabledCollection.Add(honorElite);
             }
 
             ElitesHonorEnabled = new ReadOnlyCollection<EliteDef>(honorEnabledCollection);
             ElitesHonorDisabled = new ReadOnlyCollection<EliteDef>(honorDisabledCollection);
+        }
+
+        private static void CreateBlacklistCollections(Run run)
+        {
+            var prestigeBlacklist = new List<BodyIndex>();
+            var regularBlacklist = new List<BodyIndex>();
+
+            foreach(var prestigeBody in _prestigeBodyBlacklist)
+            {
+                if (prestigeBody.bodyIndex == BodyIndex.None)
+                    continue;
+
+                if(!prestigeBody.TryGetComponent<ExpansionRequirementComponent>(out var expansionRequired))
+                {
+                    prestigeBlacklist.Add(prestigeBody.bodyIndex);
+                    continue;
+                }
+
+                if (run.IsExpansionEnabled(expansionRequired.requiredExpansion))
+                {
+                    prestigeBlacklist.Add(prestigeBody.bodyIndex);
+                }
+            }
+
+            foreach (var regularBody in _regularBodyBlacklist)
+            {
+                if (regularBody.bodyIndex == BodyIndex.None)
+                    continue;
+
+                if (!regularBody.TryGetComponent<ExpansionRequirementComponent>(out var expansionRequired))
+                {
+                    prestigeBlacklist.Add(regularBody.bodyIndex);
+                    continue;
+                }
+
+                if (run.IsExpansionEnabled(expansionRequired.requiredExpansion))
+                {
+                    prestigeBlacklist.Add(regularBody.bodyIndex);
+                }
+            }
+
+            PrestigeBodyBlacklist = new ReadOnlyCollection<BodyIndex>(prestigeBlacklist);
+            RegularBodyBlacklist = new ReadOnlyCollection<BodyIndex>(regularBlacklist);
         }
     }
 }
